01610a0e2651334f13553f1f794f7de2449ec68f
shinbaminam <shinbaminam@gmail.com>
2023-09-02 16:03:54 +0600

    api yandex

--- android/app/src/main/kotlin/com/example/flutter_application_2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_2/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.flutter_application_2
 import io.flutter.embedding.android.FlutterActivity
 import com.yandex.mapkit.MapKitFactory;
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity() {   
+    @Override
+    public void onCreate() {
+        MapKitFactory.setApiKey("1d464ce6-e63e-414c-b06d-a4d9a739ae14");    
+        super.onCreate();
+    }
 }
 
-@Override
-public void onCreate() {
-    super.onCreate();
-    MapKitFactory.setApiKey("");
-}
