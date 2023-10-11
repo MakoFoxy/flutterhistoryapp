cc2502ba49ab62a5cb0472b6c314003d37bab970
shinbaminam <shinbaminam@gmail.com>
2023-10-11 18:25:10 +0600

    full app Dev Edition Mustafin Sh

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -649,47 +649,47 @@ class HomePageState extends State<MyHomePage> {
               child: const Icon(Icons.create),
             ),
           ),
-          Positioned(
-            left: 20,
-            bottom: 0,
-            child: FloatingActionButton(
-              onPressed: () async {
-                var collRefkz = FirebaseFirestore.instance.collection('datakz');
-                var collRefru = FirebaseFirestore.instance.collection('dataru');
-                var collRefen = FirebaseFirestore.instance.collection('dataen');
+          // Positioned(
+          //   left: 20,
+          //   bottom: 0,
+          //   child: FloatingActionButton(
+          //     onPressed: () async {
+          //       var collRefkz = FirebaseFirestore.instance.collection('datakz');
+          //       var collRefru = FirebaseFirestore.instance.collection('dataru');
+          //       var collRefen = FirebaseFirestore.instance.collection('dataen');
 
-                QuerySnapshot querySnapshotkz = await collRefkz.get();
-                QuerySnapshot querySnapshotru = await collRefru.get();
-                QuerySnapshot querySnapshoten = await collRefen.get();
+          //       QuerySnapshot querySnapshotkz = await collRefkz.get();
+          //       QuerySnapshot querySnapshotru = await collRefru.get();
+          //       QuerySnapshot querySnapshoten = await collRefen.get();
 
-                List<QueryDocumentSnapshot> docskz = querySnapshotkz.docs;
-                List<QueryDocumentSnapshot> docsru = querySnapshotru.docs;
-                List<QueryDocumentSnapshot> docsen = querySnapshoten.docs;
+          //       List<QueryDocumentSnapshot> docskz = querySnapshotkz.docs;
+          //       List<QueryDocumentSnapshot> docsru = querySnapshotru.docs;
+          //       List<QueryDocumentSnapshot> docsen = querySnapshoten.docs;
 
-                List<String> autokey = [];
-                for (QueryDocumentSnapshot doc in docskz) {
-                  autokey.add(doc.id);
-                }
-                for (QueryDocumentSnapshot doc in docsru) {
-                  autokey.add(doc.id);
-                }
-                for (QueryDocumentSnapshot doc in docsen) {
-                  autokey.add(doc.id);
-                }
+          //       List<String> autokey = [];
+          //       for (QueryDocumentSnapshot doc in docskz) {
+          //         autokey.add(doc.id);
+          //       }
+          //       for (QueryDocumentSnapshot doc in docsru) {
+          //         autokey.add(doc.id);
+          //       }
+          //       for (QueryDocumentSnapshot doc in docsen) {
+          //         autokey.add(doc.id);
+          //       }
 
-                autokey.forEach((element) {
-                  collRefkz.doc(element).delete();
-                });
-                autokey.forEach((element) {
-                  collRefru.doc(element).delete();
-                });
-                autokey.forEach((element) {
-                  collRefen.doc(element).delete();
-                });
-              },
-              child: const Icon(Icons.delete),
-            ),
-          ),
+          //       autokey.forEach((element) {
+          //         collRefkz.doc(element).delete();
+          //       });
+          //       autokey.forEach((element) {
+          //         collRefru.doc(element).delete();
+          //       });
+          //       autokey.forEach((element) {
+          //         collRefen.doc(element).delete();
+          //       });
+          //     },
+          //     child: const Icon(Icons.delete),
+          //   ),
+          // ),
         ],
       ),
     );
